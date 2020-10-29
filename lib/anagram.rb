93eca8ab945122222753ddb478f33b5ea180ed6f
talryan <nataliamryan@gmail.com>
2020-10-28 18:19:10 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,20 @@
 # Your code goes here!
 
 class Anagram
-    attr_accessor :name 
+    attr_accessor :words
 
-    def initialize
+    def initialize(words)
+        @words = words
     end
-    def match
+    
+    def match(array)
+        arr = []
+array.each do |string|
+   if string.split("").sort == @words.split("").sort
+arr << string
+end
+end
+arr
+        #array.split("")
     end
 end
